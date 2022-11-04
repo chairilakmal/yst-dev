cd58a91ebcfc5c43d7d666d51fe69991cf66a452
chairilakmal <hairilakmal21@gmail.com>
2022-11-04 07:11:19 +0700

    Update input-pengeluaran.php
    static

--- admin/laporan/input-pengeluaran.php
+++ admin/laporan/input-pengeluaran.php
@@ -50,10 +50,10 @@ if (isset($_POST["submit"])) {
     $query = "INSERT INTO t_lap_keuangan (bulan,tahun,tanggal,nominal,sumber,keterangan,status)
                 VALUES ('$bulan', '$tahun', '$tanggal', '$nominal','$sumber','$keterangan','$status')";
 
-                
 
 
-   
+
+
     mysqli_query($conn, $query);
     // var_dump($query);die;
 
@@ -82,71 +82,77 @@ if (isset($_POST["submit"])) {
 <div class="content-wrapper">
     <main>
         <div class="page-title-link ml-4 mb-4">
-        <a href="laporan-bulanan.php">
+            <a href="laporan-bulanan.php">
                 <i class="nav-icon fas fa-home mr-1"></i>Dashboard admin</a> >
             <a href="laporan-bulanan.php">
                 <i class="nav-icon fas fa-donate mr-1"></i>Laporan bulanan</a> >
             <a href="input-pengeluaran.php">
-                <i class="nav-icon fas fa-cog mr-1"></i>Input pengeluaran</a> 
+                <i class="nav-icon fas fa-cog mr-1"></i>Input pengeluaran</a>
         </div>
         <div class="form-profil">
             <div class="mt-2 regis-title">
                 <h3>Input Pengeluaran</h3>
             </div>
             <form action="" enctype="multipart/form-data" method="POST">
-           
-                    <div class="form-group mt-4 mb-3">
-                        <label for="tb_bulan" class="label-txt">Bulan<span class="red-star">*</span></label>
-                        <select id="tb_bulan"name="tb_bulan">
-                        <option selected disabled="selected">Bulan</option>
-                            <option> Januari </option>
-                            <option> Februari </option>
-                            <option> Maret </option>
-                            <option> April </option>
-                            <option> Mei </option>
-                            <option> Juni </option>
-                            <option> Juli </option>
-                            <option> Agustus </option>
-                            <option> September </option>
-                            <option> Oktober </option>
-                            <option> November </option>
-                            <option> Desember </option>
-                        </select>
-                        </div>
-                        <div class="form-group mt-4 mb-3">
-                            <label for="tb_tahun" class="label-txt">Tahun<span class="red-star">*</span></label>
-                            <?php
-                            $now=date("Y");
-                            echo "<select name=tb_tahun>
-                            <option value=$now selected>$now</option>";
-                            for($thn=2012; $thn<=$now; $thn++){
-                            echo "<option value=$thn>$thn</option>";}
-                            echo "</select>";
-                            ?>
-                        </div>
-                    <div class="form-group mt-4 mb-3" id="tanggal">
-                        <label for="tb_tanggal" class="label-txt">Tanggal<span class="red-star">*</span></label>
-                        <input type="date" id="tb_tanggal" name="tb_tanggal" Required>
-                    </div>
-                        <div class="form-group mt-4 mb-3">
-                        <label for="tb_nominal" class="label-num">Nominal<span class="red-star">*</span></label>
-                       <input type="number" id="tb_nominal" name="tb_nominal" class="form-control" placeholder="Masukan nominal pemasukan" Required>     
-                    </div>
-                    <div class="form-group">
-                        <label for="tb_sumber" class="label-txt">Sumber Dana<span class="red-star">*</label>
-                        <textarea class="form-control" id="tb_sumber" name="tb_sumber" placeholder="Masukkan sumber dana" Required></textarea>
-                    </div>
-                    <div class="form-group">
-                        <label for="tb_keterangan" class="label-txt">Keterangan</label>
-                        <textarea class="form-control" id="tb_keterangan" name="tb_keterangan" placeholder="Keterangan"></textarea>
+                <div class="form-group mt-4 mb-3">
+                    <label for="tb_tipe_pengeluaran">Tipe Pengeluaran<span class="red-star">*</span></label></label>
+                    <select class="form-control" id="tb_tipe_pengeluaran" name="tb_tipe_pengeluaran" required onchange="handleType()">
+                        <option value="1" selected>Umum</option>
+                        <option value="2">Beasiswa</option>
+                    </select>
+                </div>
+                <div id="field_beasiswa" class="form-group mt-4 mb-3 d-none">
+                    <label for="tb_beasiswa">Pilih Beasiswa<span class="red-star">*</span></label></label>
+                    <select class="form-control" id="tb_beasiswa" name="tb_beasiswa" required>
+                        <option value="1" selected>Beasiswa 1</option>
+                        <option value="2">Beasiswa 2</option>
+                    </select>
+                </div>
+                <div class="form-group mt-4 mb-3" id="tgl_selesai_form">
+                    <label for="tb_tanggal" class="label-txt">Tanggal<span class="red-star">*</span></label>
+                    <input type="date" id="tb_tanggal" name="tb_tanggal" class="form-control" value="<?php echo date("Y-m-d"); ?>">
+                </div>
+
+
+                <div class="form-group mt-4 mb-3">
+                    <label for="tb_nominal" class="label-num">Nominal<span class="red-star">*</span></label>
+                    <input type="number" id="tb_nominal" name="tb_nominal" class="form-control" placeholder="Masukan nominal pemasukan" Required>
+                </div>
+                <div class="form-group">
+                    <label for="tb_sumber" class="label-txt">Sumber Dana<span class="red-star">*</label>
+                    <input type="text" class="form-control" id="tb_sumber" name="tb_sumber" placeholder="Masukkan sumber dana" Required></input>
+                </div>
+                <div class="form-group">
+                    <label for="tb_keterangan" class="label-txt">Keterangan</label>
+                    <textarea class="form-control" id="tb_keterangan" name="tb_keterangan" rows="4" placeholder="Keterangan"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="image_uploads" class="label-txt">Bukti Transfer</label>
+                    <div class="file-form">
+                        <input type="file" id="image_uploads" name="image_uploads" class="form-control">
                     </div>
                 </div>
-                <button type="submit" name="submit" value="Simpan" class="btn btn-lg btn-primary w-100 yst-login-btn border-0 mt-4 mb-4">
-                    <span class="yst-login-btn-fs">Simpan</span>
-                </button>
-            </form>
         </div>
-    </main>
+        <button type="submit" name="submit" value="Simpan" class="btn btn-lg btn-primary w-100 yst-login-btn border-0 mt-4 mb-4">
+            <span class="yst-login-btn-fs">Simpan</span>
+        </button>
+        </form>
+</div>
+
+<script>
+    var fieldType = document.getElementById("tb_tipe_pengeluaran");
+    var fieldBeasiswa = document.getElementById("field_beasiswa");
+
+    function handleType() {
+        if (fieldType.value == 2) {
+            fieldBeasiswa.classList.remove("d-none");
+        } else {
+            fieldBeasiswa.classList.add("d-none");
+        }
+    }
+</script>
+
+</main>
 </div>
 <!-- /.container-fluid -->
 <!-- /.content -->
